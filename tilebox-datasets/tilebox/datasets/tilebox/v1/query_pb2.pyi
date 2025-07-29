999ad5a7af26906cfefeb0e1767b985c334a693d
Lukas Bindreiter <lukas.bindreiter@tilebox.com>
2025-07-29 12:09:42 +0200

    Upgrade to protobuf v6

--- tilebox-datasets/tilebox/datasets/tilebox/v1/query_pb2.pyi
+++ tilebox-datasets/tilebox/datasets/tilebox/v1/query_pb2.pyi
@@ -3,7 +3,8 @@ from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from tilebox.datasets.tilebox.v1 import id_pb2 as _id_pb2
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
